89ca953cbd33c9399eecf9c3e481aef912543f75
leandrohorizon <leandro.mcontato@gmail.com>
2023-10-02 21:20:44 -0300

    Adicionando safe area e cor ao fundo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,17 +32,32 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: (const Column(
+            children: [
+              Weather(),
+              LocalTime(),
+            ],
+          ))),
+    );
+  }
+}
+
+class Weather extends StatelessWidget {
+  const Weather({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return const Column(
       children: [
-        LocalTime(),
-        Column(
-          children: [
-            Text('22 graus', style: TextStyle(fontSize: 40)),
-            Text('27/20 graus sensação de 24 graus',
-                style: TextStyle(fontSize: 12)),
-            Text('predominante nublado', style: TextStyle(fontSize: 12)),
-          ],
-        )
+        Text('22 graus', style: TextStyle(fontSize: 40)),
+        Text('27/20 graus sensação de 24 graus',
+            style: TextStyle(fontSize: 12)),
+        Text('predominante nublado', style: TextStyle(fontSize: 12)),
       ],
     );
   }
