045f2cf4a62eb4651ffcf3ac36cdd59400c5fb7e
leandrohorizon <leandro.mcontato@gmail.com>
2023-10-08 11:28:16 -0300

    Estilizando Hourly

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[300],
+      backgroundColor: Colors.blue[500],
       body: const SafeArea(
         child: Column(
           children: [
@@ -89,12 +89,18 @@ class Weather extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(Icons.cloud, size: 100, color: Colors.white),
-            Text('22 graus', style: TextStyle(fontSize: 40)),
+            Icon(
+              Icons.cloud,
+              size: 100,
+              color: Colors.white,
+            ),
+            Text(
+              '22°',
+              style: TextStyle(fontSize: 40),
+            ),
           ],
         ),
-        Text('27/20 graus sensação de 24 graus',
-            style: TextStyle(fontSize: 16)),
+        Text('27/20° sensação de 24°', style: TextStyle(fontSize: 16)),
         Text('predominante nublado', style: TextStyle(fontSize: 16)),
       ],
     );
@@ -131,68 +137,76 @@ class DayList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 150,
-          child: ListView(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            children: [
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-              Hourly(
-                time: '12:00',
-                icon: Icons.cloud,
-                temperature: '22 graus',
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.blue[300],
+          border: Border.all(
+            color: Colors.transparent,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
           ),
         ),
-      ],
+        child: Column(
+          children: [
+            SizedBox(
+              height: 250,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                  Hourly(
+                    time: '12 p.m.',
+                    icon: Icons.cloud,
+                    humidity: '30%',
+                    temperature: '22°',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -202,29 +216,49 @@ class Hourly extends StatelessWidget {
     super.key,
     required this.time,
     required this.icon,
+    required this.humidity,
     required this.temperature,
   });
 
   final String time;
   final IconData icon;
+  final String humidity;
   final String temperature;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 100,
-      height: 200,
-      color: Colors.white,
-      // border radius
+      height: 500,
       margin: const EdgeInsets.all(10),
-
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(time),
-          Icon(icon, size: 50),
-          Text(temperature),
+          Text(time, style: const TextStyle(color: Colors.white)),
+          Column(children: [
+            Icon(
+              icon,
+              size: 50,
+              color: Colors.white,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.water_drop,
+                  size: 20,
+                  color: Colors.white,
+                ),
+                Text(
+                  humidity,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ]),
+          Text(temperature, style: const TextStyle(color: Colors.white)),
         ],
       ),
     );
