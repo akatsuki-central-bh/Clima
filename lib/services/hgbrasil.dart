74947338d8e55e8ce3b40bd3aef6745dbc28d510
leandrohorizon <leandro.mcontato@gmail.com>
2023-10-22 21:38:16 -0300

    Implementa chamadas de api para hgbrasil

--- lib/services/hgbrasil.dart
+++ lib/services/hgbrasil.dart
@@ -1,18 +1,39 @@
-import 'dart:async';
 import 'dart:convert';
 
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:clima/models/forecast_model.dart';
+import 'package:clima/services/http_client.dart';
 
 class HgBrasil {
+  final IHttpClient httpClient;
+
   final token = '4d5fcf14';
   final url = 'https://api.hgbrasil.com';
 
-  Future<Map<String, dynamic>> getWeather() async {
-    final response = await http.get(Uri.parse('$url/weather?key=$token'));
+  HgBrasil({required this.httpClient});
 
-    return json.decode(response.body);
+  Future<List<ForecastModel>> getWeather(String woeid) async {
+    final response = await httpClient.get(
+      url: "$url/weather?woeid=$woeid&key=$token",
+    );
+
+    if (response.statusCode == 200) {
+      final List<ForecastModel> forecasts = [];
+
+      final body = jsonDecode(response.body);
+
+      body['results']['forecast'].map((forecast) {
+        final forecastModel = ForecastModel.fromJson(forecast);
+
+        forecasts.add(forecastModel);
+      }).toList();
+
+      return forecasts;
+
+      // return body['results']['forecast'].map((forecast) {
+      //   return ForecastModel.fromJson(forecast);
+      // }).toList();
+    } else {
+      throw Exception('Failed to load weather');
+    }
   }
 }
