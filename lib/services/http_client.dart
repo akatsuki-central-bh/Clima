ca09392693e67445598a3b24f97d357eedd6ffa7
leandrohorizon <leandro.mcontato@gmail.com>
2023-10-24 23:30:48 -0300

    Remove override

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -3,8 +3,6 @@ import 'package:http/http.dart' as http;
 class HttpClient {
   final client = http.Client();
 
-  @override
-  // url, headers, body, timeout
   Future get({
     required String url,
     required Map<String, String> headers,
