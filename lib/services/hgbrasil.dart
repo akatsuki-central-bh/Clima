ff19bd6b8f4526f02bc108e8419f7c1bc8004a1b
leandrohorizon <leandro.mcontato@gmail.com>
2023-10-25 22:53:55 -0300

    Implementa cache

--- lib/services/hgbrasil.dart
+++ lib/services/hgbrasil.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:clima/data/models/weather_model.dart';
 import 'package:clima/services/http_client.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class HgBrasil {
   final HttpClient httpClient;
@@ -11,26 +12,44 @@ class HgBrasil {
 
   final url = 'http://192.168.1.14:3000';
 
+  final cache = DefaultCacheManager();
+
   HgBrasil({required this.httpClient});
 
   Future<WeatherModel> getWeather(String woeid) async {
-    final String basicAuth =
-        'Basic ${base64Encode(utf8.encode('$username:$password'))}';
+    final String urlEndpoint = "$url/weather?woeid=$woeid";
 
-    final response = await httpClient.get(
-      url: "$url/weather?woeid=$woeid&test=true",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': basicAuth,
-      },
-    );
+    try {
+      final String basicAuth =
+          'Basic ${base64Encode(utf8.encode('$username:$password'))}';
 
-    if (response.statusCode == 200) {
-      final body = jsonDecode(response.body);
+      final response = await httpClient.get(
+        url: urlEndpoint,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': basicAuth,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+
+        cache.putFile(urlEndpoint, response.bodyBytes);
+
+        return WeatherModel.fromJson(body['results']);
+      } else {
+        throw Exception('Failed to load weather');
+      }
+    } catch (UnknownHostException) {
+      return getCacheInfo(urlEndpoint);
+    }
+  }
+
+  Future<WeatherModel> getCacheInfo(String endpoint) {
+    return cache.getSingleFile(endpoint).then((file) {
+      final body = jsonDecode(file.readAsStringSync());
 
       return WeatherModel.fromJson(body['results']);
-    } else {
-      throw Exception('Failed to load weather');
-    }
+    });
   }
 }
